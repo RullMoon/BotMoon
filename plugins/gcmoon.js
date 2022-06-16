222e5dfbc565030e52b5dfd0e8a40d3459400d09
RullMoon <107094781+RullMoon@users.noreply.github.com>
2022-06-16 11:45:30 +0700

    Update and rename gchaori.js to gcmoon.js

--- plugins/gcmoon.js
+++ plugins/gcmoon.js
@@ -2,12 +2,12 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.botwm
 let str = `
-*Group oscar 📮:  https://chat.whatsapp.com/E5l0ofPuMxG3H8ZhMRyI7e*
+*Group oscar 📮:  https://chat.whatsapp.com/GcNqdNAWqLE17kmenErzfe*
 `.trim()
-conn.sendButton(m.chat, str, `©jarotbotz`, `⋮☰ Menu`, `.menu`, m)
+conn.sendButton(m.chat, str, `©MoonBot`, `⋮☰ Menu`, `.menu`, m)
 }
-handler.help = ['oscar']
+handler.help = ['gcmoon']
 handler.tags = ['info']
-handler.command = /^oscar$/i
+handler.command = /^gcmoon$/i
 
 module.exports = handler
