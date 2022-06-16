6ad85c25ea36307a9b1af35d7cd7ce43d0d0156d
RullMoon <107094781+RullMoon@users.noreply.github.com>
2022-06-16 11:43:49 +0700

    Update inforozi.js

--- plugins/inforozi.js
+++ plugins/inforozi.js
@@ -2,20 +2,20 @@ let handler  = async (m, { conn, usedPrefix }) => {
 conn.reply(m.chat, `
 ╭═══════════════════════
 ║╭──❉ 〔 INFO OWNER 〕 ❉────── 
-║│➸ ```NAMA``` : JAROT
-║│➸ ```UMUR``` : 18thn
-║│➸ ```ASAL``` : JATIM
+║│➸ ```NAMA``` : Rull
+║│➸ ```UMUR``` : Gatau
+║│➸ ```ASAL``` : Isekai
 ║│➸ ```OFFICIAL GRUP``` :
-https://chat.whatsapp.com/EQ2R3O3ckPkJZrs1MhCtQp
-║│➸ ```ISTAGRAM``` : https://instagram.com/frnshpvirtual_
-║│➸ ```WHATSAPP``` : http://wa.me/6285850539404
+https://chat.whatsapp.com/GcNqdNAWqLE17kmenErzfe
+║│➸ ```ISTAGRAM``` : -
+║│➸ ```WHATSAPP``` : http://wa.me/6285746030186
 ╰────────❉
 `.trim(), m)
 }
 
-handler.help = ['infojarot']
+handler.help = ['inforull']
 handler.tags = ['main', 'utama']
-handler.command = /^(infojarot)$/i
+handler.command = /^(inforull)$/i
 
 handler.exp = 150
 
