93eebcaa791c056e1d02a708f1f3c1110fc80ec6
RullMoon <107094781+RullMoon@users.noreply.github.com>
2022-06-16 11:19:37 +0700

    Update bcbutton.js

--- plugins/bcbutton.js
+++ plugins/bcbutton.js
@@ -10,7 +10,7 @@ let handler  = async (m, { conn, text }) => {
   let thumb = 'https://telegra.ph/file/b344d0c10f1f7f47ed657.jpg'
   let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
   let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text )
-  for (let id of chats) /*conn.send2ButtonLoc*/conn.send2Button(id, `${text}`.trim(), `\n╭─[ *_JAROTBOTZ_* ]─✧
+  for (let id of chats) /*conn.send2ButtonLoc*/conn.send2Button(id, `${text}`.trim(), `\n╭─[ *_MOONBOTZ_* ]─✧
 ╰─────···\n${time}`, 'Owner', '.owner2', 'Menu', '.menu', /*'Donasi', '.ds'*/)
   conn.reply(m.chat, `_Berhasil mengirim pesan broadcast ke ${chats.length} chat_`, m)
 }
