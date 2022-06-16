44bf90adb77de50b1c56e29ed58769dbc674a819
RullMoon <107094781+RullMoon@users.noreply.github.com>
2022-06-16 11:32:51 +0700

    Update zifabotz3.js

--- plugins/zifabotz3.js
+++ plugins/zifabotz3.js
@@ -3,8 +3,8 @@ const PhoneNumber = require('awesome-phonenumber')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
-    "displayName": "JAROT",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:JAROT;;;\nFN:MURSID\nitem1.TEL;waid=6285850539404:6285850539404\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:-\nitem2.X-ABLabel:💌 Email\nitem3.URL:-/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ JAROT ]───────\nEND:VCARD"
+    "displayName": "Rull",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:JAROT;;;\nFN:MURSID\nitem1.TEL;waid=6285746030186:6285746030186\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:-\nitem2.X-ABLabel:💌 Email\nitem3.URL:-/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ RULL ]───────\nEND:VCARD"
   }, {
     "displayName": "Whatsapp",
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:WHASTAPP;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
@@ -14,6 +14,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
-handler.command = /^(ownerjarot)$/i
+handler.command = /^(ownerrull)$/i
 
 module.exports = handler
